36f46543f8cd443a8bf03c9a8863154292e737bf
廖莉祺 <cxliao@liaoliqideMacBook-Pro.local>
2021-09-12 22:07:26 +0800

    demo ver done

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,6 +1,6 @@
 import React,{useEffect,useState} from 'react';
 import 'antd/dist/antd.css';
-import {Layout, Menu, Breadcrumb,Modal, Button,Input,Row,Col,Card, Tabs} from 'antd';
+import {Layout, Menu, Breadcrumb,Modal, Button,Input,Row,Col,Card, Tabs,Tag} from 'antd';
 import { userBody,optionBody,questionBody,roomBody } from '../types/typeObject';
 import { useAppSelector, useAppDispatch } from './../app/hooks'
 import { selectUser,userLogin,getUser} from './../features/user/userSlice';
@@ -20,15 +20,35 @@ export default function Dashboard(){
     
     const nodeService = new NodeService();
     async function getUserRoom(){
-        await nodeService.getAllOwnRooms(user).then((e)=>(console.log(e)));
+        await nodeService.getAllOwnRooms(user).then((e)=>(setAllOwnerRooms(e)));
     }
     const roomTemplate = (roomInfo:roomBody,ind:number) => {
         const tabHeader = `Q${ind}`;
         return (
             <TabPane tab={tabHeader} key={ind}>
-                <h2></h2>
+                <h2>Question: {roomInfo.questions[0].question}</h2>
+                <h3>Answer:</h3>
+                {roomInfo.questions[0].choices.map((e,i)=>(choiceTemplate(e,i)))}
             </TabPane>
         )
+    }
+    const choiceTemplate = (choice:optionBody,ind:number) =>{
+        return (
+            <div>
+                <h4>{ind+1}) {choice.option}</h4>
+                <h5>Who choice this?</h5>
+                <div>
+                    {choice.selectedList.map((e)=>selectorTemplate(e))}
+                </div>
+            </div>
+        );
+    }
+    const selectorTemplate = (user:userBody) =>{
+        return (
+            <Tag>
+                {user.name}
+            </Tag>
+        );
     }
     useEffect(()=>{
         console.log(user);
