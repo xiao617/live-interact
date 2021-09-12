586074a212f8130bb8f247af54e82d2de7e7a5c4
廖莉祺 <cxliao@liaoliqideMacBook-Pro.local>
2021-09-12 22:47:03 +0800

    add backend schema

--- backend/src/models/room.ts
+++ backend/src/models/room.ts
@@ -1,2 +1,26 @@
 import { model, Schema } from 'mongoose';
 import { IRoom } from '../types/room';
+
+const roomSchema:Schema = new Schema(
+    {
+        id: {
+            type: String
+        },
+        roomId: {
+            type: String,
+            default: ""
+        },
+        question: {
+            type: Array,
+            default: []
+        },
+        owner: {
+            type: String,
+            default: ""
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+export default model<IRoom>('Room',roomSchema)
--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,2 +1,25 @@
 import { model, Schema } from 'mongoose';
 import { IUser } from '../types/user';
+const userSchema:Schema = new Schema(
+    {
+        id: {
+            type: String
+        },
+        name: {
+            type: String,
+            default: ""
+        },
+        status: {
+            type: String,
+            default: ""
+        },
+        score: {
+            type: Number,
+            default: 0
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+export default model<IUser>('user',userSchema)
